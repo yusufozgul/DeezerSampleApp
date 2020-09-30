3802995bc7afcf2206e501a01bee3efec2b537fa
Yusuf Özgül <yozgul@icloud.com>
2020-09-30 13:06:57 +0300

    Added coredata model and coredata manager

---
+++ DeezerSample/DataBase/DataBaseController.swift
@@ -0,0 +1,59 @@
+//
+//  DataBaseController.swift
+//  DeezerSample
+//
+//  Created by Yusuf Özgül on 30.09.2020.
+//
+
+import UIKit
+import CoreData
+
+class DataBaseController {
+    let context: NSManagedObjectContext
+    let entityName = "FavoriteTracks"
+    static let shared: DataBaseController = DataBaseController()
+    
+    private init() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+    }
+    
+    func save(data: AlbumDetailTrackListData) {
+        let entity = NSEntityDescription.entity(forEntityName: entityName, in: context)
+        let newTrack = NSManagedObject(entity: entity!, insertInto: context)
+        
+        newTrack.setValue(data.albumImage, forKey: "albumImage")
+        newTrack.setValue(data.albumName, forKey: "albumName")
+        newTrack.setValue(data.artistName, forKey: "artistName")
+        newTrack.setValue(data.duration, forKey: "duration")
+        newTrack.setValue(data.preview, forKey: "preview")
+        newTrack.setValue(data.title, forKey: "title")
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving")
+        }
+    }
+    
+    func fetch() -> [AlbumDetailTrackListData] {
+        var favorites: [AlbumDetailTrackListData] = []
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                favorites.append(AlbumDetailTrackListData(albumImage: data.value(forKey: "albumImage") as! String,
+                                                          title: data.value(forKey: "title") as! String,
+                                                          duration: data.value(forKey: "duration") as! Int,
+                                                          preview: data.value(forKey: "preview") as! String,
+                                                          artistName: data.value(forKey: "artistName") as! String,
+                                                          albumName: data.value(forKey: "albumName") as! String))
+            }
+            
+        } catch {
+            
+            print("Failed")
+        }
+        return favorites
+    }
+}
